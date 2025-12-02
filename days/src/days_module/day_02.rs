8b65a6cef3fe0801ce61422bf4f6e8e1fe475f60
dgoldsb <dgoldsb@live.nl>
2025-12-02 22:43:03 +0100

    Answer too high

--- days/src/days_module/day_02.rs
+++ days/src/days_module/day_02.rs
@@ -1,28 +1,46 @@
 use crate::days_module::day::Day;
+use std::collections::HashSet;
 
 pub struct Day02 {}
 
 fn find_invalid_ids(range: &str) -> Vec<usize> {
-    println!("{}", range);
-    let start_string = range.split('-').nth(0).unwrap();
-    let end_string = range.split('-').nth(1).unwrap();
+    let start = range.split('-').nth(0).unwrap().parse::<usize>().unwrap();
+    let end = range.split('-').nth(1).unwrap().parse::<usize>().unwrap();
 
-    // Take the first half, first half is shorter for uneven.
-    let half_start = start_string[..start_string.len() / 2].parse::<usize>().unwrap_or(start_string.parse::<usize>().unwrap());
-    let half_end = end_string[..(end_string.len() + 1) / 2].parse::<usize>().unwrap();
-    println!("{} - {}", half_start, half_end);
-
-    // Iterate over the range of half IDs and add valid ones to the returned list.
     let mut invalid_ids: Vec<usize> = Vec::new();
-    let start = start_string.parse::<usize>().unwrap();
-    let end = end_string.parse::<usize>().unwrap();
+    for candidate in start..=end {
+        let candidate_string = &candidate.to_string();
+        if candidate_string.len() % 2 == 0 {
+            if candidate_string[..candidate_string.len() / 2].to_string()
+                == candidate_string[candidate_string.len() / 2..].to_string()
+            {
+                invalid_ids.push(candidate);
+            }
+        }
+    }
 
-    for i in half_start..=half_end {
-        let invalid_id = (i.to_string() + i.to_string().as_str()).parse::<usize>().unwrap();
-        println!("{}", invalid_id);
-        if invalid_id <= end && invalid_id >= start {
-            invalid_ids.push(invalid_id);
-            println!("ADDED {}", invalid_id);
+    invalid_ids
+}
+
+fn find_all_invalid_ids(range: &str) -> Vec<usize> {
+    let start = range.split('-').nth(0).unwrap().parse::<usize>().unwrap();
+    let end = range.split('-').nth(1).unwrap().parse::<usize>().unwrap();
+
+    let mut invalid_ids: Vec<usize> = Vec::new();
+    for candidate in start..=end {
+        let candidate_string = &candidate.to_string();
+
+        for repeat_split in 1..=(candidate_string.len() + 1) / 2 {
+            let mut built_string = String::new();
+
+            while built_string.len() < candidate_string.len() {
+                built_string += &candidate_string[..repeat_split];
+            }
+
+            if &built_string == candidate_string {
+                println!("{}", candidate);
+                invalid_ids.push(candidate);
+            }
         }
     }
 
@@ -39,11 +57,23 @@ impl Day for Day02 {
     }
 
     fn part_a(&self, input: &String) -> String {
-        input.split(',').map(find_invalid_ids).flatten().sum::<usize>().to_string()
+        input
+            .split(',')
+            .map(find_invalid_ids)
+            .flatten()
+            .sum::<usize>()
+            .to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
-        "".to_string()
+        input
+            .split(',')
+            .map(find_all_invalid_ids)
+            .flatten()
+            .collect::<HashSet<usize>>()
+            .into_iter()
+            .sum::<usize>()
+            .to_string()
     }
 }
 
